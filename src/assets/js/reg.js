7bd493459b2c461bb661396d14aa802ae9c983ad
gastón_Alonso() <tonga88@live.com.ar>
2023-09-02 01:26:49 -0300

    agregue pages login y register

---
+++ src/assets/js/reg.js
@@ -0,0 +1,8 @@
+const regForm = document.querySelector('form');
+const btnForm = document.getElementById("btnSubmit");
+const inputName = document.getElementById("name");
+const inputLastName = document.getElementById("lastName");
+const inputEmail = document.getElementById("email");
+const inputPass = document.getElementById("password");
+
+
